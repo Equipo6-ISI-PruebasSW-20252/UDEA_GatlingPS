4360c5e11f33bdbfaea34da0592efa3aa7e326ad
Emanuel Munera <122242669+munera72@users.noreply.github.com>
2025-10-06 13:30:35 -0500

    Got rid of all concurrent injections

--- src/test/scala/parabank/LoginTest.scala
+++ src/test/scala/parabank/LoginTest.scala
@@ -23,10 +23,10 @@ class LoginTest extends Simulation{
   // 3 Load Scenario
   setUp(
     scn.inject(
-      rampConcurrentUsers(100).during(10), // carga normal
-      //constantConcurrentUsers(100).during(30), // mantener carga para ver luego cómo se comporta durante ese tiempo
-      //rampConcurrentUsers(100).to(200).during(10), // escalar a carga pico
-      //constantConcurrentUsers(200).during(30) // mantener la carga pico para ver su comportamiento también
+      rampUsers(100).during(10), // carga normal
+      constantUsersPerSec(100).during(30), // mantener carga para ver luego cómo se comporta durante ese tiempo
+      rampUsers(100).to(200).during(10), // escalar a carga pico
+      constantUsersPerSec(200).during(30) // mantener la carga pico para ver su comportamiento también
     )
   ).protocols(httpConf);
 
