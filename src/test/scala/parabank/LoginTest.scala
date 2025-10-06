961af295774c13df1233b55536146cca60f13d73
Emanuel Munera <122242669+munera72@users.noreply.github.com>
2025-10-06 13:08:33 -0500

    Commented 3 injections, left one only

--- src/test/scala/parabank/LoginTest.scala
+++ src/test/scala/parabank/LoginTest.scala
@@ -23,10 +23,10 @@ class LoginTest extends Simulation{
   // 3 Load Scenario
   setUp(
     scn.inject(
-      rampUsers(100).during(10),  // carga normal
-      constantConcurrentUsers(100).during(30), // mantener carga para ver luego cómo se comporta durante ese tiempo
-      rampConcurrentUsers(100).to(200).during(10), // escalar a carga pico
-      constantConcurrentUsers(200).during(30)  // mantener la carga pico para ver su comportamiento también
+      rampUsers(100).during(10), // carga normal
+      //constantConcurrentUsers(100).during(30), // mantener carga para ver luego cómo se comporta durante ese tiempo
+      //rampConcurrentUsers(100).to(200).during(10), // escalar a carga pico
+      //constantConcurrentUsers(200).during(30) // mantener la carga pico para ver su comportamiento también
     )
   ).protocols(httpConf);
 
