17eaac3d5b1a120032b0fbd29a58c093347d3c2e
Emanuel Munera <122242669+munera72@users.noreply.github.com>
2025-10-06 13:33:23 -0500

    Fixed error of non-existing method

--- src/test/scala/parabank/LoginTest.scala
+++ src/test/scala/parabank/LoginTest.scala
@@ -25,7 +25,7 @@ class LoginTest extends Simulation{
     scn.inject(
       rampUsers(100).during(10), // carga normal
       constantUsersPerSec(100).during(30), // mantener carga para ver luego cómo se comporta durante ese tiempo
-      rampUsers(100).to(200).during(10), // escalar a carga pico
+      rampUsers(200).during(10), // escalar a carga pico
       constantUsersPerSec(200).during(30) // mantener la carga pico para ver su comportamiento también
     )
   ).protocols(httpConf);
