68a374bd77ab133358a64da19d2303717fdcaca3
jose643415 <108759315+jose643415@users.noreply.github.com>
2025-10-07 15:11:32 -0500

    Update LoanRequestLoadTest.scala

--- src/test/scala/parabank/LoanRequestLoadTest.scala
+++ src/test/scala/parabank/LoanRequestLoadTest.scala
@@ -3,3 +3,24 @@ package parabank
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
+
+class LoanRequestLoadTest extends Simulation {
+
+  val feeder = csv("data/feeder.csv").circular
+  
+    // 1 Http Conf
+  val httpConf = http.baseUrl(url)
+    .acceptHeader("application/json")
+    //Verificar de forma general para todas las solicitudes
+    .check(status.is(200))
+
+  // 2 Scenario Definition 
+  val scn = scenario("Solicitud de préstamo bajo carga")
+ 
+  
+
+  
+  
+
+  
+}
