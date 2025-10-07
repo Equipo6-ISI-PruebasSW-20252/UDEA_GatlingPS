6ef1d11fa2d6b1968046f1f6e331295e1cdadd01
jose643415 <108759315+jose643415@users.noreply.github.com>
2025-10-07 16:17:57 -0500

    Update LoanRequestLoadTest.scala
    Prueba para prestamo bajo carga

--- src/test/scala/parabank/LoanRequestLoadTest.scala
+++ src/test/scala/parabank/LoanRequestLoadTest.scala
@@ -6,7 +6,7 @@ import scala.concurrent.duration._
 
 class LoanRequestLoadTest extends Simulation {
 
-  val feeder = csv("data/feeder.csv").circular
+  val feeder = csv("data/loanRequests.csv").circular
   
     // 1 Http Conf
   val httpConf = http.baseUrl(url)
@@ -16,11 +16,34 @@ class LoanRequestLoadTest extends Simulation {
 
   // 2 Scenario Definition 
   val scn = scenario("Solicitud de préstamo bajo carga")
- 
-  
+    .feed(feeder)
+    .exec(http("Request Loan - ${fromAccountId}")
+        .post("/requestloan")
+        .body(
+          StringBody(
+            """
+              {
+                "amount": ${amount},
+                "downPayment": ${downPayment},
+                "fromAccountId": ${fromAccountId}
+              }
+            """
+          )
+        ).asJson
+        .check(status.in(200, 201))
+    ).pause(1, 3)
 
-  
-  
-
-  
+  // 3 Load Scenario
+  setUp(
+    scn.inject(
+      nothingFor(5.seconds),
+      rampConcurrentUsers(0) to (150) during (30.seconds), // subida progresiva
+      constantConcurrentUsers(150) during (3.minutes),     // carga estable
+      rampConcurrentUsers(150) to (0) during (30.seconds)  // bajada
+    )
+  ).protocols(httpConf)
+    .assertions(
+      global.responseTime.mean.lte(5000),        // Tiempo promedio ≤ 5 s
+      global.successfulRequests.percent.gte(98)  // Éxito ≥ 98 %
+    )
 }
